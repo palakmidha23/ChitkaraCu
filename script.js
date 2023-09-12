ad0666dce0d1c4f335a61898684a8ffd210df33c
palakmidha23 <101326301+palakmidha23@users.noreply.github.com>
2023-09-12 09:04:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,43 @@
-const myDoc = document.getElementById("header");
-console.log(myDoc);
+let productjson=localStorage.getItem("products");
 
-//byClass
-const byClass = document.getElementsByClassName("para");
-console.log(byClass);
-
-//Tagline
-const para = document.getElementsByTagName("p");
-console.log(para);
-
-//QuerySelector  class
-const query = document.querySelector(".head");
-console.log(query);
-
-//querySelectorAll id
-const id = document.querySelectorAll("#header");
-console.log(id);
-
-const id1 = document.querySelectorAll("#header");
-console.log(id1);
-
-// DOM properties
-// 1. innerHTML
-// 2. innerText
-// 3. textContent
-
-console.log(myDoc.innerHTML);
-myDoc.innerHTML="<div>Hello <p>world!!</p></div>";
-
-console.log(myDoc.innerText);
-myDoc.innerText="class G4 learning DOm";
-
-//textContent
-console.log(myDoc.textContent);
-
-//style
-myDoc.style.color="yellow";
-myDoc.style.fontSize="50px";
-
-// get attribute
-console.log(myDoc.getAttribute("id"));
-console.log(byClass[0].getAttribute("class"));
-
-//set attribute
-myDoc.setAttribute("class","myclass");
-console.log(myDoc);
-
-//classList
-// 1. Add
-// 2. Remove
-// 3. Toggle
-
-console.log(myDoc.classList);
-console.log(myDoc.classList);
-
-const classLis=document.querySelector("#header");
-console.log(classLis.classList);
-
-classLis.classList.add("g4");
-classLis.classList.add("randomClass");
-
-const btn=document.querySelector(".btn");
-const form=document.querySelector(".myForm");
-btn.addEventListener("click",function(){
-   // form.classList.remove("hide");
-   form.classList.toggle("hide");
+let productData=JSON.parse(productjson);
+let addProduct=document.querySelector(".addProducts");
+let container=document.querySelector(".container");
+let showProduct=document.querySelector(".showProducts");
+console.log(showProduct);
+console.log(container);
+console.log(productData);
+addProduct.addEventListener("click",(ev)=>{
+    window.open("addProduct.html","popupWindow","width=50px;height=400px")
 })
+showProduct.addEventListener("click",(ev)=>{
+    showItem(productData);
+});
+function showItem(productData){
+   
+        // console.log(container);
+        container.innerHTML="";
+   
+
+for(let i=0;i<productData.length;i++){
+    let product=productData[i];
+    console.log(product);
+    let div=document.createElement("div");
+    div.classList.add("product");
+    div.innerHTML=`<div class="img-container">
+    <img src="${product.image}" alt="${product.name}">
+</div>
+<div class="product-info">
+    <p class="name">Name: ${product.name}</p>
+   
+    <p class="quantity">Quantity: ${product.quantity}</p>
+   
+    <p class="price">Price: ₹${product.price}</p>
+   
+</div>`
+
+    container.append(div);
+
+
+}
+}
